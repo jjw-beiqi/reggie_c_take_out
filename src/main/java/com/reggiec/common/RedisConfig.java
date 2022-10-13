c1cf1298675d85639f20d09009ff72d040ebc862
jjw-beiqi <jjw2816769638@126.com>
2022-10-14 00:34:03 +0800

    redis环境配置

---
+++ src/main/java/com/reggiec/common/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.reggiec.common;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+
+        // 也可以不设置序列化器，会设置默认的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+}
